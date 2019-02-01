19e0e3f136fd7177c3817f51ffbb91a03a285a78
Deepankar Sharma <dash.srmk@gmail.com>
2019-02-02 01:55:34 +0530

    Stack implementation using Queue

--- test.py
+++ test.py
@@ -10,16 +10,24 @@ class LinkedList:
     """
     def __init__(self):
         self.head = self.tail = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
 
     def is_empty(self):
+        """
+        Checks if the LinkedList is empty or not
+        :return: bool
+        """
         if self.head == self.tail is None:
             return True
         return False
 
     def insert_at_end(self, data):
         """
-        Adds a new node at the end of the Linked List
-        Insertion complexity is O(1) of this implementation
+        Adds a new node at the end of the LinkedList
+        Time Complexity: O(1)
         :param data: value you want to add at the end
         :return: None
         """
@@ -29,25 +37,33 @@ class LinkedList:
             last_node = self.tail
             last_node.next = Node(data)
             self.tail = last_node.next
+        self.size += 1
 
     def remove_from_beg(self):
+        """
+        Removes node which is at the beginning of LinkedList
+        Time Complexity: O(1)
+        :return: self.head.data
+        """
         if self.is_empty():
-            raise Exception("{} is empty".format(__class__.__name__))
+            raise Exception("Data Structure is empty")
         else:
             head_node = self.head
             self.head = head_node.next
-            del head_node
             # when the only node is removed
             if self.head is None:
                 self.tail = None
+        self.size -= 1
+        return head_node.data
 
     def traverse(self):
         """
-        Traverses the Nodes and prints Node data on STDOUT
+        Traverses the Nodes and prints data on STDOUT
+        Time Complexity: O(n)
         :return: None
         """
         if self.is_empty():
-            raise Exception("{} is empty".format(__class__.__name__))
+            raise Exception("Data Structure is empty")
         cur_node = self.head
         while cur_node:
             print(cur_node.data, end=' ')
@@ -57,21 +73,24 @@ class LinkedList:
 
 class Queue(LinkedList):
     """
-    FIFO Queue implementation using LinkedList data structure
+    Queue (FIFO) data structure implementation using singly LinkedList
     """
     def enqueue(self, data):
         return self.insert_at_end(data)
 
     def dequeue(self):
-        if self.is_empty():
-            raise Exception("{} is empty".format(__class__.__name__))
         return self.remove_from_beg()
 
-    def traverse(self):
-        if self.is_empty():
-            raise Exception("{} is empty".format(__class__.__name__))
-        super().traverse()
 
+class Stack(Queue):
+    """
+    Stack (LIFO) data structure implementation using a Queue
+    """
+    def push(self, data):
+        return self.enqueue(data)
 
-Q = Queue()
-print(dir(Q))
+    def pop(self):
+        for _ in range(self.size-1):
+            temp = self.dequeue()
+            self.enqueue(temp)
+        return self.dequeue()
